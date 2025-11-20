b86b934b2e50de42540abdd24939bf6f34045543
sci.vcruz <viniciusidacruz@hotmail.com>
2025-11-20 17:33:57 -0300

    feat: configure path aliases and update imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import * as path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@styles": path.resolve(__dirname, "./src/shared/styles"),
+      "@assets": path.resolve(__dirname, "./src/shared/assets"),
+      "@components": path.resolve(__dirname, "./src/shared/components"),
+      "@hooks": path.resolve(__dirname, "./src/shared/hooks"),
+      "@utils": path.resolve(__dirname, "./src/shared/utils"),
+      "@types": path.resolve(__dirname, "./src/shared/types"),
+      "@services": path.resolve(__dirname, "./src/shared/services"),
+    },
+  },
 });
