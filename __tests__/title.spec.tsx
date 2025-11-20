61594e3c8b60ec1968d72754381fc4c9479dc5d7
sci.vcruz <viniciusidacruz@hotmail.com>
2025-11-20 19:11:30 -0300

    fix(test): use vitest-specific jest-dom import path

---
+++ __tests__/title.spec.tsx
@@ -0,0 +1,11 @@
+import { expect, test, describe } from "vitest";
+import { render } from "@testing-library/react";
+
+describe("Title", () => {
+  test("renders title", () => {
+    const { getByRole } = render(
+      <h1 className="text-2xl font-bold">Hello World</h1>
+    );
+    expect(getByRole("heading", { level: 1 })).toHaveTextContent("Hello World");
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+import * as path from "node:path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setup-vitest.ts",
+    coverage: {
+      provider: "v8",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@styles": path.resolve(__dirname, "./src/shared/styles"),
+      "@assets": path.resolve(__dirname, "./src/shared/assets"),
+      "@components": path.resolve(__dirname, "./src/shared/components"),
+      "@hooks": path.resolve(__dirname, "./src/shared/hooks"),
+      "@utils": path.resolve(__dirname, "./src/shared/utils"),
+      "@types": path.resolve(__dirname, "./src/shared/types"),
+      "@services": path.resolve(__dirname, "./src/shared/services"),
+    },
+  },
+});
